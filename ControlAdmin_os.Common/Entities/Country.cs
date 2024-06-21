1d89cf5613cc38090369f3726d1b65f54fda240f
Antonio Martinez Gonzalez <amartinez@itesa.edu.mx>
2024-06-21 17:30:23 -0600

    Versión01. Arquitectura y conexión a la bd desde Program de Web

---
+++ ControlAdmin_os.Common/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlAdmin_os.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+
+}
